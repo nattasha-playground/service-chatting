6fd8197d8e6d464b5f500e9e34d8473b6207106b
andriepu <andriepu@gmail.com>
2017-08-03 19:05:12 +0700

    install simsimi

---
+++ chat.js
@@ -0,0 +1,15 @@
+const request = require('request')
+
+module.exports = (message, resolve) => {
+  const encodedMessage = encodeURI(message.replace(/nattasha|natt/gi, 'simi').replace(/@/g, ''))
+  const simiURL = `${process.env.CHAT_URL}${encodedMessage}`
+
+  request(simiURL, (error, resp, body) => {
+    const result = JSON.parse(body)
+    if (result.simsimi_talk_set) {
+      const simiResponse = result.simsimi_talk_set.answers[0].sentence
+      const response = simiResponse.replace(/sim |simi|simsimi|simsimk/gi, 'nattasha').replace(/ayam|anak ayam/gi, 'anak kecil')
+      resolve(response)
+    }
+  })
+}
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 // Get the packages we need
 const express = require('express')
+const request = require('request')
+const dotenv = require('dotenv')
+const chat = require('./chat')
+
+// --- Load environment variables
+dotenv.load()
 
 // Create our Express application
 const app = express()
@@ -10,9 +16,26 @@ const port = process.env.PORT || 3000
 // Create our Express router
 const router = express.Router()
 
-// http://localhost:3000/api
+// --- Root: `http://localhost:3000/api`
 router.get('/', (req, res) => {
-  res.json({ message: 'Hello, service-chatting here...' })
+  res.json({ message: 'Hello!' })
+})
+
+var chatRoute = router.route('/chat')
+
+// --- Create endpoint `/api/chat` for GET
+chatRoute.get((req, res) => {
+  const query = req.query
+
+  if (query.message) {
+    const promise = new Promise((resolve, reject) => {
+      chat(query.message, resolve)
+    }).then( (response) => {
+      res.json({ status: 'OK', message: response })
+    })
+  } else {
+    res.json({ status: 'ERROR', message: 'Invalid parameters.' })
+  }
 })
 
 // Register all our routes with /api
@@ -20,4 +43,4 @@ app.use('/api', router)
 
 // Start the server
 app.listen(port)
-console.log('Listening to port ' + port)
+console.log(`Listening to port ${port}`)
